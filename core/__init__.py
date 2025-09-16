b787b38f10ca2436222d35f497be15ca03ee12ff
陈明祯 <chenmingzhen@idea.edu.cn>
2025-09-17 01:06:40 +0800

    chore: 删除多余的代码

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,3 @@
-from core.retrieval.bm25 import BM25Retriever
 from core.retrieval.multipath import MultiPathRetriever
 from core.retrieval.dense import DenseRetriever
 from core.rerank.Reranker_Qwen3 import Qwen3Reranker
