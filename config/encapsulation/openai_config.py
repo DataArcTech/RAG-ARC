ccce23862601cd3cf6dc97a0205453547959ff58
Harvey-zhou <cs.hzhou@outlook.com>
2025-09-22 19:39:47 +0800

    remove api key

--- config/encapsulation/openai_config.py
+++ config/encapsulation/openai_config.py
@@ -8,7 +8,7 @@ class OpenAIChatConfig(AbstractConfig):
     model_name: str = "gpt-4o-mini"
     task_types: list = ["chat"]
     base_url: str = "https://api.gptsapi.net/v1"
-    api_key: str = "sk-2T06b7c7f9c3870049fbf8fada596b0f8ef908d1e233KLY2"
+    api_key: str = "sk-xxx"
     max_tokens: int = 100
     temperature: float = 0.2
 
