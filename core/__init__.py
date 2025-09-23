bf576acdafc1e2575f0a3a4847e6d9ac7ea6205f
陈明祯 <chenmingzhen@idea.edu.cn>
2025-09-23 10:49:48 +0800

    chore: clean up

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-# from core.retrieval.multipath import MultiPathRetriever
-# from core.retrieval.dense import DenseRetriever
-# from core.rerank.Reranker_Qwen3 import Qwen3Reranker
-
-# __all__ = ["BM25Retriever", "MultiPathRetriever", "DenseRetriever", "Qwen3Reranker"]
