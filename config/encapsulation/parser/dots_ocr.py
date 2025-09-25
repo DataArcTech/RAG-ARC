74e3330fd57c9fe6f5345d6f40b10246bd36c453
陈明祯 <chenmingzhen@idea.edu.cn>
2025-09-25 19:58:14 +0800

    chore

--- config/encapsulation/parser/dots_ocr.py
+++ config/encapsulation/parser/dots_ocr.py
@@ -8,8 +8,7 @@ class DotsOCRConfig(AbstractConfig):
     type: Literal["dots_ocr"] = "dots_ocr"
 
     # Model configuration
-    # model_path: str = "rednote-hilab/dots.ocr"  # Default to HuggingFace model ID, can be local path
-    model_path: str = "/finance_ML/dataarc_syn_database/model/rednote-hilab/DotsOCR"
+    model_path: str = "rednote-hilab/dots.ocr"  # Default to HuggingFace model ID, can be local path
     cache_dir: Optional[str] = None
     use_hf: bool = True
     device: str = "cuda:1"
